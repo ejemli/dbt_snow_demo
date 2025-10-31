d8f3501ce3ce816b2a27696aeb13ed8f8fb0eb3a
Emna Jemli <emna.jemli@talan.com>
2025-10-31 21:02:25 +0000

    daily weather model created

--- models/date_dimension.sql
+++ models/date_dimension.sql
@@ -4,21 +4,10 @@ select
  TO_TIMESTAMP(started_at) as started_at,
  DATE(TO_TIMESTAMP(started_at)) as date_started_at,
  HOUR(TO_TIMESTAMP(started_at)) as hour_started_at,
- 
-CASE 
-WHEN DAYNAME(TO_TIMESTAMP(started_at)) IN ('Sat', 'Sun')
-THEN 'WEEKEND'
-ELSE 'BUSINESSDAY'
-END AS day_type,
 
-CASE WHEN MONTH(TO_TIMESTAMP(started_at)) IN (12,1,2)
-    THEN 'WINTER'
-    WHEN MONTH(TO_TIMESTAMP(started_at)) IN (3,4,5)
-    THEN 'SPRING'
-    WHEN MONTH(TO_TIMESTAMP(started_at)) IN (6,7,8)
-    THEN 'SUMMER'
-    ELSE 'AUTUMN'
-    END AS station_of_year
+{{day_type('started_at')}} AS day_type,
+
+{{get_season('started_at')}} AS station_of_year
 
 from
 {{ source('demo', 'bike') }}
