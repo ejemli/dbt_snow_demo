22a433f20226f17e40f9a811f5584d2fb42ba5be
Emna Jemli <emna.jemli@talan.com>
2025-10-31 21:29:49 +0000

    trip fact model created

---
+++ models/trip_fact.sql
@@ -0,0 +1,23 @@
+with trips as (
+
+    select
+    ride_id,
+    rideable_type,
+    date(TO_TIMESTAMP(started_at)) as trip_date,
+    start_statio_id as start_station_id,
+    END_STATION_id,
+    member_csual as member_casual,
+    TIMESTAMPDIFF(SECOND,TO_TIMESTAMP(STARTED_AT),TO_TIMESTAMP(ENDED_AT)) AS TRIP_DURATION_SECONDS
+    
+    from {{ source('demo', 'bike') }}
+
+    WHERE ride_id != 'ride_id'
+
+    limit 10
+
+
+)
+
+select
+*
+from trips
